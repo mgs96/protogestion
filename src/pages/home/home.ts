39ccb22132dc2e997e017e9b244d4a1b7aa9bdb7
Mauricio Guzmán <mauricio.guzman.salazar@gmail.com>
2018-05-30 23:24:44 -0500

    initial commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,77 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker
+} from '@ionic-native/google-maps';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  map: GoogleMap;
+
+  constructor(
+    private navCtrl: NavController,
+    private googleMaps: GoogleMaps
+  ) {
+
+  }
+
+  ionViewDidLoad(){
+    this.loadMap();
+  }
+
+  loadMap(){
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+        target: {
+          lat: 43.0741904, // default location
+          lng: -89.3809802 // default location
+        },
+        zoom: 18,
+        tilt: 30
+      }
+    };
+  
+    this.map = this.googleMaps.create('map_canvas', mapOptions);
+  
+    // Wait the MAP_READY before using any methods.
+    this.map.one(GoogleMapsEvent.MAP_READY)
+    .then(() => {
+      // Now you can use all methods safely.
+      this.getPosition();
+    })
+    .catch(error =>{
+      console.log(error);
+    });
+  
+  }
+
+  getPosition(): void{
+    this.map.getMyLocation()
+    .then(response => {
+      this.map.moveCamera({
+        target: response.latLng
+      });
+      this.map.addMarker({
+        title: 'My Position',
+        icon: 'blue',
+        animation: 'DROP',
+        position: response.latLng
+      });
+    })
+    .catch(error =>{
+      console.log(error);
+    });
+  }
+
+}
